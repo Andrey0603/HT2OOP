3552eeedf6e290a37fd3a4b5528d311f0a6aa31e
Andrey <cliff_06@bk.ru>
2024-02-03 12:59:58 +0500

    Создали код в файле Main

---
+++ Main.java
@@ -0,0 +1,33 @@
+    
+    public class Main {
+        public static void main(String[] args) {
+            Participant[] participants = {
+                    new Human("Вася", 100, 2),
+                    new Human("Петя", 150, 3),
+                    new Cat("Барсик", 150, 3),
+                    new Robot("RB", 200, 4)
+            };
+    
+            Object[] obstacles = {
+                    new Treadmill(120),
+                    new Wall(2),
+                    new Treadmill(200),
+                    new Wall(3)
+            };
+    
+            for (Participant participant : participants) {
+                for (Object obstacle : obstacles) {
+                    if (obstacle instanceof Treadmill) {
+                        ((Treadmill) obstacle).overcome(participant);
+                    } else if (obstacle instanceof Wall) {
+                        ((Wall) obstacle).overcome(participant);
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
